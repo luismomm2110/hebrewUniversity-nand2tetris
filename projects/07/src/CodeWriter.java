df443bcbb7a1fb534865d1fda1d565b1d9ed048a
luismomm2110 <luismomm@gmail.com>
2021-04-07 16:07:34 -0300

    Project 7 - StackTest sucessfull

--- projects/07/src/CodeWriter.java
+++ projects/07/src/CodeWriter.java
@@ -1,8 +1,8 @@
 import java.io.*;
-//import java.util.Scanner;
 
 public class CodeWriter {
 
+    Integer numJump = 0;
     String pathAssembly;
     File assemblyTranslate;
     FileWriter assemblyWriter;
@@ -16,33 +16,45 @@ public class CodeWriter {
         } catch (FileNotFoundException e) {
             System.out.println("File not found!");
         }
-
     }
 
     public void writeArithmetic(String arg1) throws IOException {
 
         if (arg1.equals("add")) {
-            this.assemblyWriter.write("@SP\n" + "AM=M-1\n" + "M=D\n" + "@SP\n" + "AM=M-1\n" + "M=M+D\n" + "@SP\n" + "M=M+1\n");
+            this.assemblyWriter.write(backSP() + "M=D\n" + backSP() + "M=M+D\n" + advSP());
         }
 
         if (arg1.equals("sub")) {
-            this.assemblyWriter.write("@SP\n" + "M=M-1\n" + "A=M\n" + "D=M\n" +
-                    "@SP\n" + "M=M-1\n" + "A=M\n" + "D=D-M\n" + "@SP\n" + "A=M\n" + "M=D\n" +
-                    "@SP\n" + "M=M+1\n");
+            this.assemblyWriter.write(backSP() + "M=D\n" + backSP() + "M=M-D\n" + advSP());
         }
 
         if (arg1.equals("neg")) {
-
-            this.assemblyWriter.write("@SP\n" + "M=M-1\n" + "A=M\n" + "D=M\n" + "D=-D\n" +
-                     "@SP\n" + "A=M\n" + "M=D\n" +
-                    "@SP\n" + "M=M+1\n");
-
+            this.assemblyWriter.write(backSP() + "M=-M\n" + advSP());
         }
 
         if (arg1.equals("eq")) {
-            this.assemblyWriter.write("@");
+            this.assemblyWriter.write(compCommands("eq"));
         }
 
+        if (arg1.equals("gt")) {
+            this.assemblyWriter.write(compCommands("gt"));
+        }   
+        
+        if (arg1.equals("lt")) {
+            this.assemblyWriter.write(compCommands("lt"));
+        }
+
+        if (arg1.equals("and")) {
+            this.assemblyWriter.write(backSP() +"D=M\n" + backSP() + "M=M&D\n" + advSP());
+        }
+
+        if (arg1.equals("or")) {
+            this.assemblyWriter.write(backSP() +"D=M\n" + backSP() + "M=M|D\n" + advSP());
+        }
+        
+        if (arg1.equals("not")) {
+            this.assemblyWriter.write(backSP() +"M=!M\n" + advSP());
+        }
 
     }
 
@@ -52,6 +64,31 @@ public class CodeWriter {
                 this.assemblyWriter.write("@" + Integer.toString(c_index) + "\n" + "D=A\n" + "@SP\n" + "A=M\n" + "M=D\n" + "@SP\n" + "M=M+1\n");
             }
          }
+    }
+
+    private String compCommands(String type) {
+        String comparison = "\n";
+
+        if (type.equals("eq")) {
+            comparison = "D;JNE\n";
+        } else if (type.equals("gt")) {
+            comparison  = "D;JLE\n";
+        } else if (type.equals("lt")) {
+            comparison = "D;JGE\n";
+        }
+
+        numJump++;
+
+        return this.backSP() + "D=M\n" + backSP() + "D=M-D\n" + "@FALSE" + numJump + "\n" + comparison + "@SP\n" + "A=M\n" + "M=-1\n" + "@CONTINUE" + numJump
+         + "\n" + "D;JMP\n" + "(FALSE" + numJump + ")\n" + "@SP\n" + "A=M\n" + "M=0\n" + "(CONTINUE" + numJump + ")\n" + advSP();
+    }
+
+    private String backSP () {
+        return "@SP\n" + "AM=M-1\n";
+    }
+
+    private String advSP() {
+        return "@SP\n" + "M=M+1\n";
     }
 
     public void close() throws IOException {
